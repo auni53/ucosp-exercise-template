0b0386bb43940efbe031168fdcfebf10c0c89283
John Ford <john@johnford.org>
2017-09-18 15:25:33 +0200

    Template for unit tests

---
+++ test/fetch_test.js
@@ -0,0 +1,13 @@
+const assume = require('assume');
+const subject = require('../');
+
+describe('URL Fetcher', () => {
+  it('should fetch a url', async () => {
+    let result = await subject.fetch({url: 'http://httpbin.org/get'});
+    assume(result).has.property('statusCode', 200);
+    assume(result).has.property('body');
+    assume(result.body).to.be.a('Buffer');
+    // Since we're expecting JSON, let's make sure it parses
+    JSON.parse(result.body);
+  });
+});
