7e2e232230e87095168460b925274497085def29
auni53 <auni.ahsan@mail.utoronto.ca>
2017-09-22 12:30:29 -0400

    Improve fetch method
    * support http
    * add a couple of tests

--- test/fetch_test.js
+++ test/fetch_test.js
@@ -2,7 +2,7 @@ const assume = require('assume');
 const subject = require('../');
 
 describe('URL Fetcher', () => {
-  it('should fetch a url', async () => {
+  it('should fetch an https url', async () => {
     let result = await subject.fetch({url: 'https://httpbin.org/get'});
     assume(result).has.property('statusCode', 200);
     assume(result).has.property('body');
@@ -10,4 +10,20 @@ describe('URL Fetcher', () => {
     // Since we're expecting JSON, let's make sure it parses
     JSON.parse(result.body);
   });
+
+  it('should fetch an http url', async () => {
+    let result = await subject.fetch({url: 'http://httpbin.org/get'});
+    assume(result).has.property('statusCode', 200);
+    assume(result).has.property('body');
+    assume(result.body).to.be.a('Buffer');
+    JSON.parse(result.body);
+  });
+
+  it('should fail to fetch on an unsupported protocol', async () => {
+    try {
+      let result = await subject.fetch({url: 'ftp://httpbin.org/get'});
+      return Promise.reject(new Error());
+    } catch(e) { }
+  });
+
 });
